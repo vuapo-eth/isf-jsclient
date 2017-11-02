c01cea94ab60db701e63dde73f21baa1e496dad2
mikrohash.de <iota@mikrohash.de>
2017-11-02 23:14:41 +0100

    auto detect missing iota.min.js

--- js/init.js
+++ js/init.js
@@ -2,5 +2,9 @@
 
 sendAjaxRequest({mode: 'check_update', version: '0.2'}, onUpdateChecked);
 
+if(window.IOTA === undefined) {
+	$('#content').html("<h1>Sorry to interrupt you!</h1><p>We couldn't find 'js/iota.min.js'. Please follow the instructions provided in our 'README.md' file to find out what to do.</p>");
+}
+
 //nextPageButtonClicked();
 //nextPageButtonClicked();
